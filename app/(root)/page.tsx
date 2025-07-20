3d0ad8caa9af7b6360b6337b3772a0fdb3dc4a97
George-Valentin Cujbă <george.cujba15@gmail.com>
2025-07-21 02:08:51 +0300

    Configure the StartupCard and add the Sanity CMS setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
 export default async function Home({
@@ -6,6 +7,23 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: 'Adrian',
+      },
+      _id: 1,
+      description: 'This is a description.',
+      image:
+        'https://images.unsplash.com/photo-1527430253228-e93688616381?q=80&w=3134&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
 
   return (
     <>
@@ -24,8 +42,18 @@ export default async function Home({
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for ${query}` : `All Startups`}
+          {query ? `Search results for "${query}"` : `All Startups`}
         </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
