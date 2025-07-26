8260343cb533a3832d52ee4ae2203276da45f0ba
George-Valentin Cujbă <george.cujba15@gmail.com>
2025-07-26 19:58:13 +0300

    Finish the Startup Details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,19 @@
 import { STARTUP_BY_ID_QUERY } from '@/lib/queries';
+import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense } from 'react';
+import Image from 'next/image';
+
+import markdownit from 'markdown-it';
+import { Skeleton } from '@/components/ui/skeleton';
+import View from '@/components/View';
 
 export const experimental_ppr = true;
+
+// package used to parse markdown type field data received from sanity
+const md = markdownit();
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -12,9 +22,71 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   if (!post) return notFound();
 
+  const parsedContent = md.render(post?.pitch || '');
+
   return (
     <Fragment>
-      <h1 className="text-3xl">{post.title}</h1>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
+        <h1 className="heading">{post.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post.description}</p>
+      </section>
+
+      <section className="section_container">
+        <img
+          src={post.image}
+          alt="thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
+
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${post.author?._id}`}
+              className="flex gap-2 items-conter mb-3">
+              <Image
+                src={post.author.image}
+                alt="avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author.username}
+                </p>
+              </div>
+            </Link>
+
+            <p className="category-tag">{post.category}</p>
+          </div>
+
+          <h3 className="text-30-bold">Pitch Details</h3>
+
+          {parsedContent ? (
+            <article
+              className="prose max-w-4xl font-work-sans break-all"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+            />
+          ) : (
+            <p className="no-result">No details provided</p>
+          )}
+        </div>
+
+        <hr className="divider" />
+
+        {/* TODO: EDITOR SELECTED STARTUPS */}
+
+        {/* -> Whenever you want to make something dynamic in PPR
+         * -> This allows us to show a fallback in case we cannot render something new or it's something to show while that something new isn't ready to be rendered
+         */}
+        <Suspense fallback={<Skeleton className="view-skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </Fragment>
   );
 };
